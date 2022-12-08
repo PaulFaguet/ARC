5c9590dd6c3073bede4003192e7bbff88d794563
PaulFaguet <paulfaguet7@gmail.com>
2022-12-08 14:46:59 +0100

    table for use cases examples

--- openai_test.py
+++ openai_test.py
@@ -1,12 +1,13 @@
 import streamlit as st 
 import os
 import openai 
+import pandas as pd 
 
 # DEV
-# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # PROD
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+# openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 st.title('OPEN AI')
 
@@ -14,20 +15,34 @@ col1, col2 = st.columns(2)
 sub_col1, sub_col2 = st.columns(2)
 
 with col1:
-    language = st.radio("Choisissez une langue :", ('Français', 'Anglais', 'Italien', 'Allemand', 'Espagnol', 'Portugais'))
+    language = st.multiselect("Choisissez une langue :", ('Allemand', 'Anglais', 'Danois', 'Espagnol', 'Finlandais', 'Français', 'Italien', 'Norvégien', 'Néerlandais', 'Portugais', 'Russe', 'Suédois'), default=['Français'],)
 with col2:
     algo = st.radio("Choisissez un algorithme :", ('text-davinci-003', 'text-curie-001', 'text-babbage-001', 'text-ada-001'), help="DaVinci es le plus polyvalent, Curie est utile pour le ML et l'analyse prédictive, Babbage est utile pour l'analyse de données et le traitement, Ada est utile pour l'automatisation de tâches complexes https://beta.openai.com/docs/models/gpt-3")
 
 with sub_col1:
-    words_number = st.slider("Choisissez le nombre de mots à générer :", 50, 2000, (250, 750), 50)
+    words_number = st.slider("Choisissez le nombre de mots à générer :", 50, 2000, (250, 750), 50, help="Un token correspond plus ou moins à une syllabe. 'Chat' = 1 token, 'Montagne' = 3 tokens, 'Sarkozy' = 4 tokens car mot peu commun.")
 
 with sub_col2:
-    temperature = st.slider("Température :", 0.0, 1.0, 0.5, 0.05)
-st.write("""
-         Une température plus élevée signifie que le modèle prendra plus de risques. 
-         Essayez 0,9 pour des applications plus créatives et 0 pour celles avec une réponse bien définie. \n
-         Avec une température de 0.9, il est probable que les résultats soient en anglais.
-         """)
+    temperature = st.slider("Température :", 0.0, 1.0, 0.5, 0.05, help="Une température plus élevée signifie que le modèle prendra plus de risques. Essayez 0.9 pour des applications plus créatives et 0 pour celles avec une réponse bien définie. Avec une température de 0.9, il est probable que les résultats soient en anglais.")
+
+
+df_examples = pd.DataFrame(
+    columns=['Utilisation', 'Saisie'],
+    data=[
+        ['Créateur de liste de mots-clés', 'Liste 10 mots-clés en rapport avec le vin et les caves à vin'],
+        ['Générateur d\'idées/Brainstorming', 'Brainstorm des idées de contenu en rapport avec le vin et les caves à vin'],
+        ['Génération d\'une FAQ', 'Crée une liste de 10 questions fréquemment posées sur les caves à vin'],
+        ['Utilisation d\'un langage plus ou moins soutenu', 'Ecrit un texte comme un humain à propos des caves à vins'],
+        ['Générateur de structure de texte', 'Crée un plan de dissertation en rapport avec les caves à vin et leurs détails techniques'],
+        ['Générateur de texte à partie de mots/phrases clé(e)s', 'Ecrit un texte en rapport avec les caves à vin en te basant sur ces mots-clés : cave, vin, température, humidité, Ma Cave à Vin, blog, passion, etc.'],
+        ['Extracteur de mots-clés', 'Extrait les mots clés de ce texte : [entrer un texte]'],
+        ['Traducteur', 'Traduit ce texte en 1. anglais, 2. espagnol, et 3. portugais : [entrer un texte]'],
+        ['Résumé de texte', 'Résume ce texte : [entrer un texte]'],
+        ['Utilisation d\'un pronom différent', 'Ecrit un texte à propos des caves à vins en utilisant le pronom "je"'],
+    ], 
+    index=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+)
+st.table(df_examples)
 
 st.write("Voici la page des exemples d'utilisation de l'API de OpenAI : https://beta.openai.com/examples")
 
