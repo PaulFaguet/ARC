eb73b0d6dd97877ebe42541f34fe052d7b7576d3
PaulFaguet <paulfaguet7@gmail.com>
2023-03-10 10:40:38 +0100

    added mp4

--- pages/whisper.py
+++ pages/whisper.py
@@ -17,19 +17,20 @@ audio_input = st.file_uploader(
     type=['mp3', 'mp4']
 )
 
+# st.success("Type du fichier : %s" % audio_input[0].type)
 if audio_input is not None and st.button('Transcrire'): 
     i = 1
     for audio_file in audio_input:
         # check if the type is mp3 or mp4
         audio_file_path = st.secrets["AUDIO_FILE_PATH_MP3"] if audio_file.type == "audio/mpeg" else st.secrets["AUDIO_FILE_PATH_MP4"]
         st.info("Fichier audio n°%s : %s" % (i, audio_file.name))
-        if audio_file.type == "audio/mpeg":
+        if audio_file.type == "audio/mpeg" or audio_file.type == "video/mp4":
             with open(audio_file_path, "wb") as f:
                 f.write(audio_file.getbuffer())
                 transcript = openai.Audio.transcribe("whisper-1", open(audio_file_path, 'rb'))["text"]
                 st.write(transcript)
                 # delete the file
-            os.remove("fichier.mp3")
+            os.remove(audio_file_path)
         i += 1
     
     st.success("La transcription a été réalisée avec succès.")
