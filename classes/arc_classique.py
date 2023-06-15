64ff4019c6cb6c14ef42e80f03a14c32351292cf
Paul Faguet <paul.faguet@axess.fr>
2023-06-15 17:31:48 +0200

    bricolage du fichier json des prompts sur arc classique

--- classes/arc_classique.py
+++ classes/arc_classique.py
@@ -3,7 +3,8 @@ import openai
 
 class ARC_Classique:
     def __init__(self):
-        self.df_examples = pd.read_json('classes\prompt_examples.json')
+        # self.df_examples = pd.read_json(r'classes\prompt_examples.json')
+        pass
         
     def create_prompt(self, user_text_input: str, user_word_number_input: int):
         prompt = f"{user_text_input}. Tu es un rédacteur dans une agence de référencement web qui rédige des textes pour soigner le SEO. Le texte se doit d'être intelligible et de respecter les consignes fournies ci-dessus. Le texte doit être d'environ {user_word_number_input} mots."
